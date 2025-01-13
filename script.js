c7e6e1b8b8185e14672625de087b0034db19ef2a
Jeremy <wjgrice@gmail.com>
2025-01-13 11:28:14 -0600

    Updated to flipping transition as cards are clicked.

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ let currentScore = 000000;
 let hiScore = 000000;
 let tileStartColor = 'rgba(234, 197, 234, 0.35)';
 
-
 // Set hiScore
 updateHiScore();
 
@@ -34,20 +33,12 @@ const COLORS = [
 ];
 
 // Helper function to shuffle an array
-// it returns the same array with values shuffled
-// it is based on an algorithm called Fisher Yates if you want ot research more
 function shuffle(array) {
   let counter = array.length;
 
-  // While there are elements in the array
   while (counter > 0) {
-    // Pick a random index
     let index = Math.floor(Math.random() * counter);
-
-    // Decrease counter by 1
     counter--;
-
-    // And swap the last element with it
     let temp = array[counter];
     array[counter] = array[index];
     array[index] = temp;
@@ -58,128 +49,138 @@ function shuffle(array) {
 
 let shuffledColors = shuffle(COLORS);
 
-// Loops over the array of colors
-// it creates a new div and gives it a class with the value of the color
-// it also adds an event listener for a click for each card
 function createDivsForColors(colorArray) {
   for (let color of colorArray) {
+    const tile = document.createElement('div');
+    tile.classList.add(color, 'tile');
+    tile.setAttribute('matchState', 'no match');
 
-    // create a new div
-    const newDiv = document.createElement("div");
-    newDiv.style.backgroundColor = tileStartColor;
+    const front = document.createElement('div');
+    front.classList.add('front');
 
-    // give it a class attribute for the value we are looping over and to query select latter
-    newDiv.classList.add(color);
-    newDiv.classList.add("tile")
+    const back = document.createElement('div');
+    back.classList.add('back');
 
-    // give it a two custom attributes to indicate flipped status and if it's matched or not
-    newDiv.setAttribute('matchState', 'no match');    
+    tile.append(front, back);
+    tile.addEventListener('click', handleCardClick);
 
-    // call a function handleCardClick when a div is clicked on
-    newDiv.addEventListener("click", handleCardClick);
-    
-    // append the div to the element with an id of game
-    gameContainer.append(newDiv);
+    gameContainer.append(tile);
   }
 }
 
 // Reset game when New Game btn pressed
 document.querySelector('.startbtn').addEventListener('click', resetGame);
 function resetGame() {
-  currentScore = 000000;
-  gameTiles = document.querySelector('.tile-container').children;
-  for (let i = 0; i < gameTiles.length; i++) {
-    gameTiles[i].style.backgroundColor = tileStartColor;
-    gameTiles[i].setAttribute('matchState', 'no match'); 
+  currentScore = 0;
+  seconds = 0;
+  localStorage.removeItem('clickCounter');
+  document.querySelector('.score').innerText = "Score: 000000";
+
+  gameContainer.innerHTML = '';
+  shuffledColors = shuffle(COLORS);
+  createDivsForColors(shuffledColors);
+}
+
+function handleCardClick(event) {
+  let target = event.target.closest('.tile');
+  if (!target || target.classList.contains('flip')) return;
+
+  let matchState = target.getAttribute('matchState');
+  let color = target.className.split(" ")[0];
+
+  if (gameState === 'unPaused' && matchState === 'no match') {
+    let currentFlip = flipNum();
+
+    if (currentFlip === '1st') {
+      target.classList.add('flip');
+      target.querySelector('.front').style.backgroundColor = color;
+      prevTarget = target;
+    } else if (currentFlip === '2nd') {
+      target.classList.add('flip');
+      target.querySelector('.front').style.backgroundColor = color;
+
+      if (target.querySelector('.front').style.backgroundColor === prevTarget.querySelector('.front').style.backgroundColor) {
+        target.setAttribute('matchState', 'matched');
+        prevTarget.setAttribute('matchState', 'matched');
+        score();
+        updateHiScore();
+        checkWinCondition(); // Check if all tiles are matched
+      } else {
+        setTimeout(() => {
+          target.classList.remove('flip');
+          prevTarget.classList.remove('flip');
+        }, timer);
+        gameState = 'paused';
+        delay(timer).then(() => (gameState = 'unPaused'));
+      }
+    }
   }
 }
 
-
-// TODO: Implement this function!
-function handleCardClick(event) {
-  let target = event.target;
-  let classes = target.className.split(" ");
-  let color = classes[0];
-  let matchState = target.getAttribute('matchState');
-
-  if (gameState === 'unPaused') {  // Check if game is paused
-    let currentFlip = flipNum(); // check if this click is the 1st or 2nd click
-    if (currentFlip === '1st' && matchState === 'no match'){ //Is this the 1st click and does the tile have a "match tag"
-      target.style.backgroundColor = color; // show tile by changing color
-      target.style.opacity = '100%';
-      prevTarget = target; // setup process for tracking previous clicks
-    }
-    if (currentFlip === '2nd' && matchState === 'no match') { // Check if 2nd flip and not a previously match tile
-      target.style.backgroundColor = color; // show tile by changing color
-      target.style.opacity = '100%';
-      if (target.style.backgroundColor === prevTarget.style.backgroundColor) { // Check is current tile and prev tile match by color
-        target.setAttribute('matchState', 'matched'); // set current and prev tiles match states.
-        prevTarget.setAttribute('matchState', 'matched');
-        score(); // Up current score
-        updateHiScore();
-      }
-      else {  // no match... flip the two tiles back over by deleting background color and pause for length of timer variable
-        setTimeout(function() {
-          target.style.backgroundColor = tileStartColor;
-          prevTarget.style.backgroundColor = tileStartColor;
-        }, timer);
-        gameState = 'paused'
-        delay(timer).then(() => gameState = 'unPaused');  //magic
-      }
-    }
+function checkWinCondition() {
+  const unmatchedTiles = document.querySelectorAll('.tile[matchState="no match"]');
+  if (unmatchedTiles.length === 0) {
+    celebrate();
   }
-} //Func
+}
 
-// Start/reset game
+function celebrate() {
+  const confettiContainer = document.createElement('div');
+  confettiContainer.classList.add('confetti-container');
+  document.body.appendChild(confettiContainer);
+
+  for (let i = 0; i < 100; i++) {
+    const confetti = document.createElement('div');
+    confetti.classList.add('confetti');
+    confetti.style.left = Math.random() * 100 + 'vw';
+    confetti.style.animationDelay = Math.random() * 2 + 's';
+    confetti.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+    confettiContainer.appendChild(confetti);
+  }
+
+  setTimeout(() => {
+    confettiContainer.remove();
+  }, 5000);
+}
+
 function updateHiScore() {
   if (localStorage.getItem('hiScore') === null) {
-    localStorage.setItem('hiScore', currentScore)
-  }
-  else if (localStorage.getItem('hiScore') < currentScore) {
-    localStorage.setItem('hiScore', currentScore)
+    localStorage.setItem('hiScore', currentScore);
+  } else if (localStorage.getItem('hiScore') < currentScore) {
+    localStorage.setItem('hiScore', currentScore);
   }
   let storedScore = localStorage.getItem('hiScore');
   document.querySelector('.hi-score').innerText = "High Score: " + storedScore;
 }
 
-
-// Do no completely understand this code but I needed a timer to pause the game.  
-// Borrowed from: https://masteringjs.io/tutorials/fundamentals/wait-1-second-then
 function delay(time) {
   return new Promise(resolve => setTimeout(resolve, time));
 }
 
 function score() {
   if (seconds <= 20) {
-    currentScore += seconds*1000*3.14;
-  }
-  else if (seconds <= 40) {
-    currentScore += seconds*500*3.14;
-  }
-  else if (seconds <= 60) {
-    currentScore += seconds*250*3.14;
-  }
-  else {
-    currentScore += seconds*100*3.14;
+    currentScore += seconds * 1000 * 3.14;
+  } else if (seconds <= 40) {
+    currentScore += seconds * 500 * 3.14;
+  } else if (seconds <= 60) {
+    currentScore += seconds * 250 * 3.14;
+  } else {
+    currentScore += seconds * 100 * 3.14;
   }
   let scoreBoard = document.querySelector('.score');
   scoreBoard.innerText = "Score: " + currentScore;
 }
 
-
-//Simple counter using local storage returns 0 or 1.
 function flipNum() {
   if (localStorage.getItem('clickCounter') === null) {
-    localStorage.setItem('clickCounter', '1st')
-  }
-  else if (localStorage.getItem('clickCounter') === "1st") {
-    localStorage.setItem('clickCounter', '2nd')
-  }
-  else if (localStorage.getItem('clickCounter') === "2nd") {
-    localStorage.setItem('clickCounter', '1st')
+    localStorage.setItem('clickCounter', '1st');
+  } else if (localStorage.getItem('clickCounter') === "1st") {
+    localStorage.setItem('clickCounter', '2nd');
+  } else if (localStorage.getItem('clickCounter') === "2nd") {
+    localStorage.setItem('clickCounter', '1st');
   }
   let counter = localStorage.getItem('clickCounter');
-  return (counter)
+  return counter;
 }
 
 // when the DOM loads
